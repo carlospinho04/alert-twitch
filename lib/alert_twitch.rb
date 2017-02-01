9bf6d1ee584b949fed2e76e0503da1306ac7ade5
Carlos Pinho <kakasp04@gmail.com>
2017-02-01 16:55:27 +0000

    some improves

--- lib/alert_twitch.rb
+++ lib/alert_twitch.rb
@@ -6,7 +6,8 @@ config_data = GetConfig.new
 config_data.read_file("config.json")
 twitch_info = GetTwitchInfo.new config_data.client_id, config_data.twitch_id 
 message = MessageSender.new
-while(1)
+loop do
   message.send_message(twitch_info.get_streamers_online,config_data.twilio_number, config_data.phone_number, config_data.account_sid, config_data.oauth_token)
   sleep(300)
 end
+
